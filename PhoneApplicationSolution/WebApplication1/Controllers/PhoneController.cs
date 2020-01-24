c1bf3dcae2254af68e2a35e6e8f062db3a1961e2
Daniel Pedroza <danielc.pedrozal@gmail.com>
2020-01-23 20:32:48 -0500

    Completed Lab 3

---
+++ PhoneApplicationSolution/WebApplication1/Controllers/PhoneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            List<Phone> PhoneList = new List<Phone>
+            {
+               new Phone{ PhoneID = 0001,PhoneName = "iAmBad", Manufacturer = "Pear", msrp = 999.99,ScreenSize = "5.8 inch", DateReleased = DateTime.Parse("03/11/2017") },
+               new Phone{ PhoneID = 0002,PhoneName = "Solar System Notation 8", Manufacturer = "Samuelsings", msrp = 274.99,ScreenSize = "6.3 inch", DateReleased = DateTime.Parse("23/07/2017") },
+               new Phone{ PhoneID = 0003,PhoneName = "Noodle Bit 4", Manufacturer = "Noodle", msrp = 1259.99,ScreenSize = "6.3 inch", DateReleased = DateTime.Parse("24/10/2019") }
+            };
+            return View(PhoneList);
+        }
+    }
+}
---
+++ PhoneApplicationSolution/WebApplication1/Models/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Phone
+    {
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public double msrp { get; set; }
+        public string ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+        public Phone()
+        {
+        }
+    }
+}
